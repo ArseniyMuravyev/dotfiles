675011031314c0112cb2274c0d10b152e649662a
Arseniy Muravyev <arseniymuravyev@Arseniys-MacBook-Air.local>
2024-08-14 10:10:40 +0300

    fixed

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -69,7 +69,7 @@ return {
       require("nvim-tree").setup {
         filters = {
           dotfiles = false,
-          custom = { "node_modules", ".DS_Store", ".git" },
+          custom = { "node_modules", ".DS_Store", ".git", ".next" },
         },
         view = {
           width = 24,
