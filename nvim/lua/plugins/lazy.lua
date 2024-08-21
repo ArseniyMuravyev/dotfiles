75eb04c215c566814daf5e05b0e9b5ca943dd925
Arseniy Muravyev <arseniymuravyev@Arseniys-MacBook-Air.local>
2024-08-21 14:38:39 +0300

    fixed

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -195,15 +195,14 @@ require("lazy").setup({
 			},
 		},
 	},
-		{
-			"romgrk/barbar.nvim",
-			dependencies = {
-				"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-			},
-			init = function()
-				vim.g.barbar_auto_setup = false
-			end,
-			opts = {
-			},
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
 		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {},
+	},
 })
