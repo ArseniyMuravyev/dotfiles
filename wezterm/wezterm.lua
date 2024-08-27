578f6d777e53003fba199b8219c909630eb7248f
Arseniy Muravyev <arseniymuravyev@Arseniys-MacBook-Air.local>
2024-08-27 08:07:07 +0300

    fixed

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Galaxy",
+	color_scheme = "Kanagawa (Gogh)",
 	enable_tab_bar = false,
 	font_size = 21.0,
 	font = wezterm.font("JetBrains Mono"),
-	macos_window_background_blur = 30,
 	initial_cols = 105,
 	initial_rows = 30,
-	window_background_opacity = 0.86,
+	macos_window_background_blur = 30,
+	window_background_opacity = 0.9,
 	window_decorations = "RESIZE",
 	keys = {
 		{
@@ -27,6 +27,16 @@ return {
 			event = { Up = { streak = 1, button = "Left" } },
 			mods = "CTRL",
 			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+		{
+			event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+			mods = "NONE",
+			action = wezterm.action.ScrollByLine(-3),
+		},
+		{
+			event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+			mods = "NONE",
+			action = wezterm.action.ScrollByLine(3),
 		},
 	},
 }
