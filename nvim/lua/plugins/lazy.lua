e8e4560f480f50246123c194b53e891c00f39496
Mursey <a@mursey.online>
2024-10-14 07:42:44 +0300

    fixed

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -174,16 +174,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {},
-	},
-	{
 		"rcarriga/nvim-notify",
 		opts = {
 			timeout = 1500,
@@ -225,6 +215,52 @@ require("lazy").setup({
 				enable = false,
 			}
 		end,
+	},
+	{
+		"b0o/incline.nvim",
+		dependencies = { "craftzdog/solarized-osaka.nvim" },
+		event = "BufReadPre",
+		priority = 1200,
+		config = function()
+			local colors = require("solarized-osaka.colors").setup()
+			require("incline").setup({
+				highlight = {
+					groups = {
+						InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+						InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+					},
+				},
+				window = { margin = { vertical = 0, horizontal = 1 } },
+				hide = {
+					cursorline = true,
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					if vim.bo[props.buf].modified then
+						filename = "[+] " .. filename
+					end
+
+					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+					return { { icon, guifg = color }, { " " }, { filename } }
+				end,
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+			{ "<S-x>", "<Cmd>bdelete<CR>", desc = "Close current buffer" },
+		},
+		opts = {
+			options = {
+				mode = "buffers",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
 	},
 	"themaxmarchuk/tailwindcss-colors.nvim",
 	"folke/zen-mode.nvim",
